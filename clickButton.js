4ce352bcadd60105323be17d85bcf52269961f7d
hiooy91 <68943110+hiooy91@users.noreply.github.com>
2020-09-09 23:52:19 +0900

    Add files via upload

---
+++ clickButton.js
@@ -0,0 +1,20 @@
+//var button1 = document.getElementById("menu-about");
+//var button2 = document.getElementById("menu-skill");
+//var button3 = document.getElementById("menu-career");
+//var button4 = document.getElementById("menu-contact");
+
+
+
+//
+//button1.addEventListener("click", function(){
+//    button1.style.color = "red";
+//});
+//button2.addEventListener("click", function(){
+//    button2.style.color = "red";
+//});
+//button3.addEventListener("click", function(){
+//    button3.style.color = "red";
+//});
+//button4.addEventListener("click", function(){
+//    button4.style.color = "red";
+//});
